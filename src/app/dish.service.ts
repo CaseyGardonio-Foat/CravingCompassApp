4b9b184d2aca35abc0f8c80366c7bc3a156ef4a5
Casey <casey.gardonio@foat.org>
2021-01-17 00:48:54 -0600

    added additional food images for dish-tionary; updated photos in dish.service.ts

--- src/app/dish.service.ts
+++ src/app/dish.service.ts
@@ -10,9 +10,9 @@ export class DishService {
     '../assets/photos/croissant_pexels-pixabay-2135.jpg',
     ['french', 'breakfast', 'bakery', 'cafe']),
     new Dish('paella', 'Spanish dish of rice mixed with vegetables and proteins--often seafood, chicken, and/or sausage--cooked over a fire in large flat pan.',
-    '../assets/photos/paella_pexels-joshua-miranda-4305836.jpg', ['spanish', 'dinner']),
+    '../assets/photos/paella_pexels-joshua-miranda-4305836_square.jpg', ['spanish', 'dinner']),
     new Dish('tortilla', 'Spanish omlet-like dish consisting of potatoes and onions bound with eggs. Other ingredients are sometimes added. Also known as "tortilla espanola" or "tortilla de patatas".',
-    '../assets/photos/tortillaEspanola_wikipedia.jpg', ['spanish', 'tapas']),
+    '../assets/photos/tortillaEspanola_chase-daley-mFfLHPr6ZZs-unsplash_square.jpg', ['spanish', 'tapas']),
   ];
 
   constructor() { }
