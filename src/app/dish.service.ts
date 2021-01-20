108344f2defc4962d7e45737d2a76f19b1661ba2
Casey <casey.gardonio@foat.org>
2021-01-20 15:05:22 -0600

    added photos and entries to dish-tionary; added styling to dish-tionary template

--- src/app/dish.service.ts
+++ src/app/dish.service.ts
@@ -7,12 +7,20 @@ import { Dish } from './dish.model';
 export class DishService {
   dishes: Dish[] = [
     new Dish('croissant', 'Flaky French laminated pastry, often shaped in the form of a crescent.', 
-    '../assets/photos/croissant_pexels-pixabay-2135.jpg',
+    '../assets/photos/croissant_pexels-pixabay-2135_masked.png',
     ['french', 'breakfast', 'bakery', 'cafe']),
-    new Dish('paella', 'Spanish dish of rice mixed with vegetables and proteins--often seafood, chicken, and/or sausage--cooked over a fire in large flat pan.',
-    '../assets/photos/paella_pexels-joshua-miranda-4305836_square.jpg', ['spanish', 'dinner']),
-    new Dish('tortilla', 'Spanish omlet-like dish consisting of potatoes and onions bound with eggs. Other ingredients are sometimes added. Also known as "tortilla espanola" or "tortilla de patatas".',
-    '../tortillaEspanola_chase-daley-mFfLHPr6ZZs-unsplash_square', ['spanish', 'tapas']),
+    new Dish('paella', 'Traditional Spanish dish of saffron-seasoned rice cooked with a variety of proteins and/or vegetables in a large, shallow pan known as a paellera.',
+    '../assets/photos/paella_pexels-joshua-miranda-4305836_masked.png', ['Europe', 'Mediterranean', 'Spanish']),
+    new Dish('Tortilla', 'Spanish-style omelet consisting of sliced potatoes and onions bound with whisked eggs. Additional ingredients, such as fresh vegetables, chorizo, or jamon are sometimes added.',
+    '../assets/photos/tortillaEspanola_mediterraneanliving_masked.png', ['Europe', 'Mediterranean', 'Spanish', 'tapas']),
+    new Dish('Croquetas', 'Popular Spanish snack, often served as a tapa, consisting of a crispy, deep-fried shell surrounding a creamy bechamel-based filling and flavored with jamon, chicken, bacalao, mushrooms, etc.', '../assets/photos/croquetas_cookidoo-thermomix_masked.png', ['Europe', 'Mediterranean', 'Spanish', 'croquettes', 'tapas']),
+    new Dish('Patatas Bravas', 'Popular Spanish tapa of crispy fried potato cubes doused in a firey red "brava" tomato- and paprika-based sauce, and often served with garlicky aioli.', '../assets/photos/patatasBravas_chase-daley-mFfLHPr6ZZs-unsplash_masked.png', ['Europe', 'Mediterranean', 'Spanish', 'tapas']),
+    new Dish('Gazpacho', 'Famous chilled vegetable soup originating in Andalucia and made from olive oli and bread pureed with tomatoes, onions, cucumbers, and other fresh vegetables and served with a variety of garnishes.', '../assets/photos/gazpacho_chase-daley-mFfLHPr6ZZs-unsplash_masked.png', ['Europe', 'Mediterranean', 'Spanish']),
+    new Dish('Poke', 'Traditional salad of fresh, raw fish or seafood--traditionally ahi tuna or octopus--mixed with vegetables, herbs, condiments, and seasoning. Can be served alone as an appetizer or with rice as a full meal.', '../assets/photos/poke_pexels-ania-rude-6169445_masked.png', ['North American', 'USA', 'Hawaiian']),
+    new Dish('Kalua Pig', 'Whole, salt-seasoned pig, wrapped with banana or ti leaves, and slow-roasted in an underground pit heated by lava rocks.', '../assets/photos/kaluaPork_KingsHawaiianBakery_masked.png', ['North American', 'USA', 'Hawaiian']),
+    new Dish('Laulau', 'Hawaiian dish of pork, butterfish, or other proteins wrapped in luau (taro) leaves and steamed, traditionally in an underground oven heated by lava rocks. Often served with rice and/or poi.', '../assets/photos/lauLau_foodland_masked.png', ['North American', 'USA', 'Hawaiian']),
+    new Dish('Shave Ice', 'Popular Hawaiian dessert of shaved (not crushed) ice flavored with fruit syrups and garnished with a variety of toppings.', '../assets/photos/shaveIce_chase-daley-mFfLHPr6ZZs-unsplash_masked.png', ['North American', 'USA', 'Hawaiian', 'dessert']),
+    
   ];
 
   constructor() { }
