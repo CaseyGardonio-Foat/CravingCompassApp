b4e4979d63d4deefe283cb3d9cc3b9d3c9211962
Casey <casey.gardonio@foat.org>
2021-01-23 22:34:32 -0600

    new nav items for landing page with working router links; adjusted spacing and sizing for landing page nav-items

--- src/app/browse-cuisine/browse-cuisine.component.ts
+++ src/app/browse-cuisine/browse-cuisine.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { DishService } from '../dish.service';
+import { Dish } from '../dish.model';
 
 @Component({
   selector: 'app-browse-cuisine',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./browse-cuisine.component.css']
 })
 export class BrowseCuisineComponent implements OnInit {
+  @Input() dishArray: Dish[]
 
-  constructor() { }
+  constructor(private dishService: DishService) { }
 
   ngOnInit(): void {
+  }
+
+  onSelected() {
+    this.dishArray = this.dishService.getKeywordDishes('keyword');
   }
 
 }
