082bc6cb776b9e45ef16373a60bc1879623a8187
Casey <casey.gardonio@foat.org>
2021-01-25 17:51:21 -0600

    added additional regional icons for Americas

--- src/app/cuisine.service.ts
+++ src/app/cuisine.service.ts
@@ -6,13 +6,13 @@ import { Cuisine } from './cuisine.model';
 })
 export class CuisineService {
   cuisines: Cuisine[] = [
-    new Cuisine('Americas', 'imagePath: string', 'continent', [], ['North America', 'Central America/Caribbean', 'South America']),
-    new Cuisine('North America', 'imagePath: string', 'region', ['Americas'], ['Canada', 'USA', 'Mexico']),
-    new Cuisine('Canada', 'imagePath: string', 'country', ['North America'], []),
-    new Cuisine('USA', 'imagePath: string', 'country', ['North America'], ['Southern', 'Barbeque', 'Hawaiian', 'Southwestern', 'Jewish', 'Puerto Rican']),
-    new Cuisine('Mexico', 'imagePath: string', 'country', ['North America'], []),
-    new Cuisine('Central America/Caribbean', 'imagePath: string', 'region', ['Americas'], ['Belize', 'Costa Rica', 'El Salvador', 'Guatemala', 'Honduras', 'Nicaragua', 'Panama', 'Jamaica', 'Puerto Rico', 'Cuba']),
-    new Cuisine('South America', 'imagePath: string', 'region', ['Americas'], ['Argentina', 'Brazil', 'Peru', 'Venezuela']),
+    new Cuisine('Americas', '../assets/foodIcons/Regions/Americas.svg', 'continent', [], ['North America', 'Central America/Caribbean', 'South America']),
+    new Cuisine('North America', '../assets/foodIcons/Regions/NorthAmerica.svg', 'region', ['Americas'], ['Canada', 'USA', 'Mexico']),
+    new Cuisine('Canada', '../assets/foodIcons/Regions/Americas.svg', 'country', ['North America'], []),
+    new Cuisine('USA', '../assets/foodIcons/Regions/Americas.svg', 'country', ['North America'], ['Southern', 'Barbeque', 'Hawaiian', 'Southwestern', 'Jewish', 'Puerto Rican']),
+    new Cuisine('Mexico', '../assets/foodIcons/Regions/Americas.svg', 'country', ['North America'], []),
+    new Cuisine('Central America/Caribbean', '../assets/foodIcons/Regions/CentralAmerica_Caribbean.svg', 'region', ['Americas'], ['Belize', 'Costa Rica', 'El Salvador', 'Guatemala', 'Honduras', 'Nicaragua', 'Panama', 'Jamaica', 'Puerto Rico', 'Cuba']),
+    new Cuisine('South America', '../assets/foodIcons/Regions/SouthAmerica.svg', 'region', ['Americas'], ['Argentina', 'Brazil', 'Peru', 'Venezuela']),
     new Cuisine('Europe', 'imagePath: string', 'continent', [], ['French', 'Spanish', 'Greek', 'German']),
     new Cuisine('French', '../assets/foodIcons/French_Croissant.svg', 'country', ['Europe'], []),
     new Cuisine('German', '../assets/foodIcons/German_BeerHall.svg', 'country', ['Europe'], []),
