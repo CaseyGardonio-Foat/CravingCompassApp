612e554be0e85cb8160ebdac416db0aba8ece5fe
Casey <casey.gardonio@foat.org>
2021-01-27 21:51:54 -0600

    added more photos to assets.photos; removed unnecessary test from app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,10 +22,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('CravingCompassApp');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('CravingCompassApp app is running!');
-  });
+  // it('should render title', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.nativeElement;
+  //   expect(compiled.querySelector('.content span').textContent).toContain('CravingCompassApp app is running!');
+  // });
 });
