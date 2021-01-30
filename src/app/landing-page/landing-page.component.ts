ce13cc8db47cfefb158e837a0686cd7850310481
Casey <casey.gardonio@foat.org>
2021-01-29 22:11:34 -0600

    added routerLink to submit button on landing page

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -8,6 +8,14 @@ import { NgForm } from '@angular/forms';
 })
 export class LandingPageComponent implements OnInit {
   @ViewChild('f') searchForm: NgForm;
+  defaultDistance: number = 5;
+  // defaultTime: string = 'Now';
+  userSearch = {
+    dishName: '',
+    location: '',
+    distance: '',
+    time: ''
+  }
 
   constructor() { }
 
@@ -15,7 +23,15 @@ export class LandingPageComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log(this.searchForm);
+    this.userSearch.dishName = this.searchForm.value.dishName;
+    this.userSearch.location = this.searchForm.value.location;
+    this.userSearch.distance = this.searchForm.value.distance;
+    this.userSearch.time = this.searchForm.value.time;
+    //n.b. this value is coming in as undefined--check API to find which datatype to submit for this
+    console.log(this.userSearch);
+    this.searchForm.reset();
+
+
   }
 
 }
