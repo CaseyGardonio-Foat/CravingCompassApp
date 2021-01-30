fcd52ed9b4c0fd93e96f15e8463ac47722b5b578
Casey <casey.gardonio@foat.org>
2021-01-30 12:15:44 -0600

    added routing to submit button on landing page

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -8,14 +8,6 @@ import { NgForm } from '@angular/forms';
 })
 export class LandingPageComponent implements OnInit {
   @ViewChild('f') searchForm: NgForm;
-  defaultDistance: number = 5;
-  // defaultTime: string = 'Now';
-  userSearch = {
-    dishName: '',
-    location: '',
-    distance: '',
-    time: ''
-  }
 
   constructor() { }
 
@@ -23,15 +15,7 @@ export class LandingPageComponent implements OnInit {
   }
 
   onSubmit() {
-    this.userSearch.dishName = this.searchForm.value.dishName;
-    this.userSearch.location = this.searchForm.value.location;
-    this.userSearch.distance = this.searchForm.value.distance;
-    this.userSearch.time = this.searchForm.value.time;
-    //n.b. this value is coming in as undefined--check API to find which datatype to submit for this
-    console.log(this.userSearch);
-    this.searchForm.reset();
-
-
+    console.log(this.searchForm);
   }
 
 }
