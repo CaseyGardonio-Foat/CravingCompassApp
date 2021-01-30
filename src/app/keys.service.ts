03b8d4ae75d159ece128f2ebb278e479aa6a65f7
Casey <casey.gardonio@foat.org>
2021-01-30 12:02:57 -0600

    added new keys.service w/gitignore to test security of remote repository

---
+++ src/app/keys.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class KeysService {
+  const: string = "superSecretInfo";
+  
+  constructor() { }
+
+
+}
