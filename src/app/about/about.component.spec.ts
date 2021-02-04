f27e3f9ed92837b87fa0c3a8cd615ec874183592
Casey <casey.gardonio@foat.org>
2021-02-03 22:08:23 -0600

    added successful unit test to about.component

--- src/app/about/about.component.spec.ts
+++ src/app/about/about.component.spec.ts
@@ -3,23 +3,30 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AboutComponent } from './about.component';
 
 describe('AboutComponent', () => {
-  let component: AboutComponent;
-  let fixture: ComponentFixture<AboutComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  // let component: AboutComponent;
+  // let fixture: ComponentFixture<AboutComponent>;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
       declarations: [ AboutComponent ]
     })
-    .compileComponents();
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AboutComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  // beforeEach(() => {
+  //   // fixture = TestBed.createComponent(AboutComponent);
+  //   component = fixture.componentInstance;
+  //   fixture.detectChanges();
+  // });
+
+  it('should create the component', () => {
+    let fixture = TestBed.createComponent(AboutComponent);
+    let app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should display text content in the About component', () => {
+    let fixture = TestBed.createComponent(AboutComponent);
+    let app = fixture.debugElement.componentInstance;
+    let compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('p').textContent).toBeTruthy();
   });
 });
