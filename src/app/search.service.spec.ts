d6e08a6ffd07672130c6efd7e942759bfb0219aa
Casey <casey.gardonio@foat.org>
2021-02-03 21:38:42 -0600

    started adding unit tests; added HttpClientTesting module to search.service.spec.ts

--- src/app/search.service.spec.ts
+++ src/app/search.service.spec.ts
@@ -1,14 +1,36 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
 
 import { SearchService } from './search.service';
 
 describe('SearchService', () => {
   let service: SearchService;
 
+  //Angular CLI-created test; n.b. HttpClientTestingModule imported above
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(SearchService);
   });
+
+  /* first attempt at Anya's solution
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      declarations: [SearchService]
+    }).compileComponents();
+    service = TestBed.inject(SearchService);
+  });
+  */
+
+  /* second attempt at Anya's solution
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      declarations: [SearchService]
+    }).compileComponents();
+  });
+  */
 
   it('should be created', () => {
     expect(service).toBeTruthy();
