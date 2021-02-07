d80a1c67541914ae7e322d9e5eb9685639e3f667
Casey <casey.gardonio@foat.org>
2021-02-07 15:25:08 -0600

    added map showing restaurant and search locations

--- src/app/restaurants/map/map.component.ts
+++ src/app/restaurants/map/map.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { KeysService } from 'src/app/keys.service';
+import { SearchService } from 'src/app/search.service';
 // import { AgmCoreModule } from '@agm/core';
 
 @Component({
@@ -11,16 +12,45 @@ import { KeysService } from 'src/app/keys.service';
 export class MapComponent implements OnInit {
   documenuKey: string;
   mapsKey: string;
-  lat: number = 40.688072;
-  lng: number = -73.997385;
+  lat: number;
+  lng: number;
+  map: any;
+  restaurantsList: any[];
+  restaurantAddressesString: string;
 
   constructor(
     private http: HttpClient,
-    private keysService: KeysService) { }
+    private keysService: KeysService,
+    private searchService: SearchService) { }
 
   ngOnInit(): void {
     this.documenuKey = this.keysService.getDocumenuKey();
     this.mapsKey = this.keysService.getMapsKey();  
+    this.lat = this.searchService.searchLat;
+    this.lng = this.searchService.searchLng;
+    this.restaurantsList = this.searchService.getRestaurantResults();
+    this.formatRestaurantAddresses();
+    console.log(this.restaurantAddressesString);
   };
-  
+
+  formatRestaurantAddresses() {
+    let restaurantAddressesString = '';
+    for(let restaurant of this.restaurantsList){
+      restaurantAddressesString += `||${restaurant.geo.lat},${restaurant.geo.lon}`
+    }
+    this.restaurantAddressesString = restaurantAddressesString;
+  };
+
+  getMap() {
+    let mapsKey = this.mapsKey;
+    let lat = this.lat;
+    let lng = this.lng;
+    return this.http
+    .get(`https://www.mapquestapi.com/staticmap/v5/map?key=${mapsKey}&locations=${lat},${lng}|circle-sm-f05423&zoom=14&size=350,225@2x`)
+    .subscribe(mapResult=> {
+      this.map = mapResult;
+      console.log('map request sent');
+    })
+  }
+
 }
