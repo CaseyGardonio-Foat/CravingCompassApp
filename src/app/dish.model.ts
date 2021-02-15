11dc0d3761878270c85f095cbf5fe5ae2ab80dbe
Casey <casey.gardonio@foat.org>
2021-02-15 15:06:53 -0600

    added background colors to remaining dish-tionary entries

--- src/app/dish.model.ts
+++ src/app/dish.model.ts
@@ -3,9 +3,9 @@ export class Dish {
     public description: string;
     public imagePath: string;
     public keyWords: string[];
-    public bgColor: string;
+    public bgColor?: string;
 
-    constructor(name: string, description: string, imagePath: string, keyWords: string[], bgColor: string){
+    constructor(name: string, description: string, imagePath: string, keyWords: string[], bgColor?: string){
         this.name = name;
         this.description = description;
         this.imagePath = imagePath;
