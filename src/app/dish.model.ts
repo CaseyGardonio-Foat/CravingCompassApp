c9f8d7fe7d78a7546fb99c893c7d0ee7d3e37ecc
Casey <casey.gardonio@foat.org>
2021-02-15 14:44:10 -0600

    added logic to dish-tionary html file to change bg colors of dishes; added property to dish.model and dish.service to assign background colors to each dish

--- src/app/dish.model.ts
+++ src/app/dish.model.ts
@@ -3,11 +3,13 @@ export class Dish {
     public description: string;
     public imagePath: string;
     public keyWords: string[];
+    public bgColor: string;
 
-    constructor(name: string, description: string, imagePath: string, keyWords: string[]){
+    constructor(name: string, description: string, imagePath: string, keyWords: string[], bgColor: string){
         this.name = name;
         this.description = description;
         this.imagePath = imagePath;
         this.keyWords = keyWords;
+        this.bgColor = bgColor;
     }
 }
